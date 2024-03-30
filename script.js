e8c8b935fc6b64e0e0543f6df7aec327e50d71a4
andrew shin <shinandy22@gmail.com>
2024-03-30 16:31:06 -0700

    Finalized HTML, CSS. Saving data upcoming...

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
+function toggleRead (index) {
+    myLibrary[index].toggleRead();
+    render();
+}
+
+
 function addBookToLibrary() { //3
     let title = document.querySelector("#title").value;    
     let author = document.querySelector("#author").value;    
@@ -28,13 +38,13 @@ newBookbtn.addEventListener("click", function(){   //1, SHOW MODAL
 
 
 
-document.querySelector("#modal").addEventListener("submit", function(event){ //2 `xx
+document.querySelector("#modal").addEventListener("submit", function(event){ //2 
     addBookToLibrary();  
     modal.close();
     event.preventDefault(); // *IMPORTAANT, see Project Description*
 
 })
-
+    
 function render() { //4
     let libraryEl = document.querySelector(".library");
     libraryEl.innerHTML = ""; // clear innerHTML so no dupes
@@ -42,14 +52,19 @@ function render() { //4
         console.log(myLibrary[i]);
         let book = myLibrary[i]
         let bookEl = document.createElement('div');
+        bookEl.classList.add('bookCard');
         bookEl.innerHTML = `
-            <div class = "bookCard">
                 <div class = "title">${book.title}</div>
                 <div class = "author"> by ${book.author}</div>
-                <div class = "pages">${book.pages}</div>
-                <button class = "readStatus">${book.read}</div>
-            </div>
+                <div class = "pages">${book.pages} Pages</div>
+                <button class = "readStatus" onclick = "toggleRead(${i})">${book.read ? "Read" : "Not Read"}</div>
+                <button class = "remove-btn" onclick = "removeBook(${i})">X</button>
             `;
-        libraryEl.append(bookEl);
+        libraryEl.appendChild(bookEl);
     }
+}
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    render();
 }
